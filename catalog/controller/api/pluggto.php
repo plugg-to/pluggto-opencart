b5365ff1604b8f0e89185ea9ddc735f9ad8e4d0f
Reginaldo Junior <76regi@gmail.com>
2016-01-30 00:32:22 -0200

    pluggto api module

---
+++ catalog/controller/api/pluggto.php
@@ -0,0 +1,20 @@
+<?php
+class ControllerApiPluggto extends Controller {
+	public function index() {
+		$json = ['is_gay' => $this->isGay('victor gazotti')];
+
+		if (isset($this->request->server['HTTP_ORIGIN'])) {
+			$this->response->addHeader('Access-Control-Allow-Origin: ' . $this->request->server['HTTP_ORIGIN']);
+			$this->response->addHeader('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+			$this->response->addHeader('Access-Control-Max-Age: 1000');
+			$this->response->addHeader('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+	public function teste($name) {
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode('teste'));
+	}
+}
