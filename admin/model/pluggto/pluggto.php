532a8d187abd0e98de0b4513ac1aa9bf86e087fd
Reginaldo Junior <76regi@gmail.com>
2016-01-31 13:59:51 -0200

    news fixes

--- admin/model/pluggto/pluggto.php
+++ admin/model/pluggto/pluggto.php
@@ -214,8 +214,18 @@ class ModelPluggtoPluggto extends Model{
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+      
+      if (isset($params['grant_type']) && !empty($params['grant_type'])) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+      } else {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . json_encode($params))
+        );
+        echo json_encode($params);
+      }
 
     }elseif (strtolower ( $method ) == "put") {
 
