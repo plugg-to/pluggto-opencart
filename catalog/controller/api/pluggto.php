22b60491c1563519096547a758f69a8c8620820d
Reginaldo Thirdlevel <reginaldo.junior@thirdlevel.com.br>
2017-06-14 18:01:52 +0000

    pluggto.php edited online with Bitbucket

--- catalog/controller/api/pluggto.php
+++ catalog/controller/api/pluggto.php
@@ -1141,7 +1141,7 @@ class ControllerApiPluggto extends Controller {
 		
 		$offset = 0;
 		
-		if (isset($this->request->get['offset'];)) {
+		if (isset($this->request->get['offset'])) {
 			$offset = $this->request->get['offset'];
 		}
 
